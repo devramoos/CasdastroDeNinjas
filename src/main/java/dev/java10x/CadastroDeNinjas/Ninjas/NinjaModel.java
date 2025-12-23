01f29b269497c404725f1b5a81314e8e44930ec6
devramoos <rodrigojgramos@outlook.com>
2025-12-23 00:05:11 -0300

    Adicionando Lombok para facilitar criação de contrutores e getter e setter.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,9 +1,15 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -20,37 +26,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") //Foreing Key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String name, String email, int idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
